3435d9f34b77a6404318d03e757a130d2191b7d3
Blake <blake_db99@outlook.com>
2020-09-25 00:47:21 -0400

    Changed the main method of BuddyInfo. Adding parameter and variables to the class: name, address, phoneNumber

---
+++ src/BuddyInfo.java
@@ -0,0 +1,22 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+    }
+
+    public static void main(String[] args) {
+
+        BuddyInfo person1 = new BuddyInfo("Desmond","Ottawa", "61322" );
+        person1.name = "Desmond";
+        System.out.println("Hello " + person1.getName() + "!");
+    }
+
+    //Getter
+    private String getName() {
+        return name;
+    }
+
+}
