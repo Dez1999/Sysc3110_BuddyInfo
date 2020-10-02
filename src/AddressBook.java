5d05276556ac1b75b83d08fc539d201055ae69ba
Blake <blake_db99@outlook.com>
2020-10-01 21:41:56 -0400

    Refactored Code more

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -7,34 +7,42 @@ import java.util.*;
  */
 public class AddressBook {
 
-    private HashMap<String, BuddyInfo> address;
+    //private HashMap<String, BuddyInfo> address;
+    private ArrayList<BuddyInfo> myBuddyAddress;
+
+    public AddressBook(){
+        myBuddyAddress = new ArrayList<>();
+
+    }
 
     public void main(String[]args) {
-        address = new HashMap<>();
-
         //System.out.print("Address book");
         BuddyInfo buddy1 = new BuddyInfo("Des", "Carleton", "229");
         AddressBook addressBook = new AddressBook();
-        addressBook.addBuddy("buddy1", buddy1);
-        addressBook.removeBuddy("buddy1");
+        addressBook.addBuddy(buddy1);
+        addressBook.removeBuddy(0);
     }
 
     /**
-     * Method: addBuddy(). Adds a buddy into Address Book Map
-     * @param buddy The name of the Buddy (Key)
+     * Method: addBuddy(). Adds a buddy into Address Book List
      * @param info The info of the buddy (Value)
      */
-    private void addBuddy(String buddy, BuddyInfo info)
+    private void addBuddy(BuddyInfo info)
     {
-        address.put(buddy, info);
+        if(info != null){
+            myBuddyAddress.add(info);
+        }
     }
 
     /**
-     * Method: removeBuddy(). Removes a buddy from the Address Book Map
-     * @param buddy The name of the Buddy (Key)
+     * Method: removeBuddy(). Removes a buddy from the Address Book List
+     * @param index The index of the Buddy
      */
-    private void removeBuddy(String buddy){
-        address.remove(buddy);
+    private BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < myBuddyAddress.size()){
+            return myBuddyAddress.remove(index);
+        }
+        return null;
     }
 
 }
